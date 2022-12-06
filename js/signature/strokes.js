0064d681030b8cb2f3d71f12f3d50abdfde4f29a
Jorge V. Contreras <contreras.jorgev@gmail.com>
2022-12-06 15:14:36 -0600

    Review License Preamble (#38)
    * Review License preamble.

--- js/signature/strokes.js
+++ js/signature/strokes.js
@@ -1,7 +1,7 @@
 ﻿/*
  * Copyright (c) ASNA, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the
+ * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  * 
  *    Copyright (c) 2012 Willow Systems Corp http://willow-systems.com
