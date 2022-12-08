6551f53f3775302155201e53f5e423dcb196ef8d
Jorge V. Contreras <contreras.jorgev@gmail.com>
2022-12-08 16:14:29 -0600

    Add JobHandle query parameter on each post request. (#39)

--- js/ajax/ajax-fetch.js
+++ js/ajax/ajax-fetch.js
@@ -7,28 +7,29 @@
 
 export { Fetch };
 
+const JOB_HANDLE_KEY = 'JobHandle';
+const HIDDEN_FIELD_NAME = {
+    JOB_HANDLE = '__ASNA_JobHandle__',
+    ASP_NET_ANTI_FORGERY_TOKEN = '__RequestVerificationToken'
+};
+
 class Fetch {
 
     static fetchWithTimeout(url, data, timeout) {
         return new Promise((resolve, reject) => {
             // Set timeout timer
-            let timer = setTimeout(
+            const timer = setTimeout(
                 () => reject(new Error('Request timed out')),
                 timeout
             );
 
-            const aspNetReqVerElements = document.getElementsByName('__RequestVerificationToken'); // ASP.NET Anti-forgery Token
-            let aspNetReqVerificationValue = '';
-            if (aspNetReqVerElements && aspNetReqVerElements.length>0) {
-                aspNetReqVerificationValue = aspNetReqVerElements[0].value;
-            }
-
+            url = Fetch.addJobHandle(url);
             fetch(url, {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    'RequestVerificationToken': aspNetReqVerificationValue
+                    'RequestVerificationToken': Fetch.getHiddenFieldValue(HIDDEN_FIELD_NAME.ASP_NET_ANTI_FORGERY_TOKEN)
                 },
                 body: JSON.stringify(data)
             }).then(
@@ -36,6 +37,34 @@ class Fetch {
                 err => reject(err)
             ).finally(() => clearTimeout(timer));
         });
+    }
+
+    static getHiddenFieldValue(fieldName) {
+        const fields = document.getElementsByName(fieldName);
+        if (fields && fields.length > 0) {
+            return fields[0].value;
+        }
+        return '';
+    }
+
+    static addJobHandle(url) {
+        if (url.search(`${JOB_HANDLE_KEY}=`) > 0)
+            return url;
+
+        const jobHandle = Fetch.getHiddenFieldValue(HIDDEN_FIELD_NAME.JOB_HANDLE);
+        if (!jobHandle) {
+            return url;
+        }
+
+        const queryKeyVal = `${JOB_HANDLE_KEY}=${jobHandle}`;
+
+        url = url.replace(/#$/, '');
+
+        if (url.search(/\\?/) > 0) {
+            return url + '&' + queryKeyVal;
+        }
+
+        return url + '?' + queryKeyVal;
     }
 }
 
